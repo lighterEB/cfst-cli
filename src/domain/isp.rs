98507abaf36807581d758b522ab4ebc7dff4a80d
lighterEB <hellohus726@126.com>
2026-02-03 01:08:40 +0800

    pref: change some code structure

--- src/domain/isp.rs
+++ src/domain/isp.rs
@@ -1,5 +1,19 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Isp {
     Ct,   // 中国电信
     Cmcc, // 中国移动
     Cu,   // 中国联通
+    Cn,   // 多线/三网
+}
+
+impl Isp {
+    /// 返回映射中文名，用于拼接 remark
+    pub fn to_name(self) -> &'static str {
+        match self {
+            Isp::Cmcc => "移动",
+            Isp::Ct => "电信",
+            Isp::Cu => "联通",
+            Isp::Cn => "三网",
+        }
+    }
 }
--- src/domain/model.rs
+++ src/domain/model.rs
@@ -1,5 +1,17 @@
-
+use super::isp::Isp;
+#[derive(Debug, Clone)]
 pub struct IpInfo {
     ip: String,
+    isp: Isp,
     remark: String,
+}
+
+impl IpInfo {
+    pub fn new(ip: String, isp: Isp, remark: Option<String>) -> Self {
+        let remark = match remark {
+            None => "CF优选".to_string(),
+            Some(remark) => remark,
+        };
+        Self { ip, isp, remark }
+    }
 }
